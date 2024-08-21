2aabec8779bb5fc1dd6290a887a2f387dd0fbdb6
Abdullah khidr <Abdullahkhidrmain@gmail.com>
2024-08-21 12:29:06 +0300

    fetch newest books use case

--- lib/features/home/domain/usecases/fetch_newest_books_use_case.dart
+++ lib/features/home/domain/usecases/fetch_newest_books_use_case.dart
@@ -1,14 +1,16 @@
 import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/use_cases/use_case_no_param.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:bookly/features/home/domain/repositories/home_repository.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchNewestBooksUseCase {
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>> {
   final HomeRepository homeRepository;
 
   FetchNewestBooksUseCase(this.homeRepository);
 
+  @override
   Future<Either<Failure, List<BookEntity>>> call() async {
-    return homeRepository.fetchNewestBooks();
+    return await homeRepository.fetchNewestBooks();
   }
 }
