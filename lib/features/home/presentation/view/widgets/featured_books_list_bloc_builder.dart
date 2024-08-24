c99ccc48b2da390794e8fe1d10c66c1b10cd26d0
Abdullah khidr <Abdullahkhidrmain@gmail.com>
2024-08-24 20:44:45 +0300

    create scroll listener of featured books and fetch more books

--- lib/features/home/presentation/view/widgets/featured_books_list_bloc_builder.dart
+++ lib/features/home/presentation/view/widgets/featured_books_list_bloc_builder.dart
@@ -3,8 +3,39 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bookly/features/home/presentation/view/widgets/featured_books_list.dart';
 import 'package:flutter/material.dart';
 
-class FeaturedBooksListBlocBuilder extends StatelessWidget {
+class FeaturedBooksListBlocBuilder extends StatefulWidget {
   const FeaturedBooksListBlocBuilder({super.key});
+
+  @override
+  State<FeaturedBooksListBlocBuilder> createState() =>
+      _FeaturedBooksListBlocBuilderState();
+}
+
+class _FeaturedBooksListBlocBuilderState
+    extends State<FeaturedBooksListBlocBuilder> {
+  int nextPage = 1;
+  late final ScrollController _scrollController;
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    var maxScrollExtent = _scrollController.position.maxScrollExtent;
+    var pixels = _scrollController.position.pixels;
+    if (pixels >= maxScrollExtent * .7) {
+      BlocProvider.of<FeaturedBooksCubit>(context)
+          .fetchFeaturedBooks(pageNumber: nextPage++);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
